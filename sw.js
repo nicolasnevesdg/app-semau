4a9ceeb6c88a51077237a80e2c2e69dfd50e8468
nicolasnevesdg <onicolasneves@gmail.com>
2026-09-01 01:56:53 -0300

    Remove agenda duplicada do painel admin

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'semau-v166-camera-overlay';
+const CACHE_NAME = 'semau-v167-admin-sem-agenda';
 
 // Aqui listamos todos os arquivos que queremos salvar no celular da pessoa
 const assetsToCache = [
