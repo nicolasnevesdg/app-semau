aa83dcabe788d801d635fdd5d1556f9ee7c8e8a3
nicolasnevesdg <onicolasneves@gmail.com>
2026-09-15 16:05:13 -0300

    chore: atualizar arte alternativa do Instagram

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'semau-v205-instagram-substituto';
+const CACHE_NAME = 'semau-v206-nova-arte-instagram';
 
 const TELAO_OFFLINE_ASSETS = [
     'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js',
