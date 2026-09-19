b3404d80c4eea3cd37f63c1879bdabe890a33b52
nicolasnevesdg <onicolasneves@gmail.com>
2026-09-19 12:48:17 -0300

    style: ampliar e reposicionar urna do sorteio

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'semau-v216-visual-sorteio';
+const CACHE_NAME = 'semau-v221-posicao-sorteio-final';
 
 const TELAO_OFFLINE_ASSETS = [
     'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js',
