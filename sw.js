89f17fb8b55af0d02f97ff632a74077db3efdf42
nicolasnevesdg <onicolasneves@gmail.com>
2026-09-19 11:02:25 -0300

    chore: atualizar logos dos patrocinadores

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'semau-v210-espacamento-boas-vindas';
+const CACHE_NAME = 'semau-v211-logos-patrocinadores';
 
 const TELAO_OFFLINE_ASSETS = [
     'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js',
