78720af55bbb9e793998fa197876a64a06721ba4
nicolasnevesdg <onicolasneves@gmail.com>
2026-09-19 13:11:59 -0300

    fix: reequilibrar slide de sorteios

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'semau-v222-urna-recortada';
+const CACHE_NAME = 'semau-v223-layout-sorteio-equilibrado';
 
 const TELAO_OFFLINE_ASSETS = [
     'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js',
