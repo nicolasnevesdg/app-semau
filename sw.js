4502466e0e6229059a39a204acd4c50f62e08be3
nicolasnevesdg <onicolasneves@gmail.com>
2026-09-19 23:47:22 -0300

    fix: alinhar texto do slide de sorteios

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'semau-v223-layout-sorteio-equilibrado';
+const CACHE_NAME = 'semau-v224-alinhamento-sorteio';
 
 const TELAO_OFFLINE_ASSETS = [
     'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js',
