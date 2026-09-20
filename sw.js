dacc808aa460d34a74fb0e300366d1a2c081c28a
nicolasnevesdg <onicolasneves@gmail.com>
2026-09-20 02:24:51 -0300

    feat: atualizar artes dos slides do telao

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'semau-v227-tipografia-lojinha';
+const CACHE_NAME = 'semau-v228-artes-telao-atualizadas';
 
 const TELAO_OFFLINE_ASSETS = [
     'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js',
